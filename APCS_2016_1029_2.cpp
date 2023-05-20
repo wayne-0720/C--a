838295eb583832be4e5d7feb18df867504978aa4
FushengOwO <ymps60425@gamil.com>
2023-05-20 16:44:42 +0800

    APCS new update

--- APCS_2016_1029_2.cpp
+++ APCS_2016_1029_2.cpp
@@ -1,40 +1,38 @@
 #include <bits/stdc++.h>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main()
 {
     int n, m, tmp, msum = 0;
-    vector<int> v;
     vector<int> a;
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
     {
+        vector<int> v;
         for (int j = 0; j < m; j++)
         {
             cin >> tmp;
             v.push_back(tmp);
         }
-        for (int s = 0; s < (int)v.size() - 1; s++)
-        {
-            for (int a = 0; a < s - 1 - i; a++)
-            {
-                if (v[a] > v[a + 1])
-                {
-                    int tmp1 = v[a];
-                    v[a] = v[a + 1];
-                    v[a + 1] = tmp1;
-                }
-            }
-        }
+        sort(v.begin(), v.end());
         msum += v[(int)v.size() - 1];
         a.push_back(v[(int)v.size() - 1]);
     }
     cout << msum << endl;
-    for (int i = (int)a.size() - 1; i > 0; i--)
+    reverse(a.begin(), a.end());
+    for (int i = (int)a.size() - 1; i >= 0; i--)
     {
         if (msum % a[i] == 0)
         {
-            cout << a[i] << " ";
+            if (i == (int)a.size() - 1 )
+            {
+                cout << a[i];
+            }else
+            {
+                cout << " " << a[i];
+            }
+            
         }
         else
         {
