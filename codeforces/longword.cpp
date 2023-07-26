b940c65b90874adab9981a8fcef38aa87f4c67c4
FushengOwO <ymps60425@gamil.com>
2023-07-26 22:53:49 +0800

    longword accepted

--- codeforces/longword.cpp
+++ codeforces/longword.cpp
@@ -1,14 +1,30 @@
-#include<bits/stdc++.h>
-#include<string>
+#include <bits/stdc++.h>
+#include <string>
 using namespace std;
 int main()
 {
-    string b[1000];
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string b[100];
     int a;
     cin >> a;
-    for (int i = 0; i < a;i++)
+    for (int i = 0; i < a; i++)
     {
         cin >> b[i];
     }
-    
+    for (auto &c : b)
+    {
+        a--;
+        if (c.length() <= 10)
+        {
+            cout << c << "\n";
+        }
+        else
+        {
+            cout << c[0] << (c.length() - 2) << c[c.length() - 1] << "\n";
+        }
+        if (a == 0)
+            break;
+    }
+    return 0;
 }
